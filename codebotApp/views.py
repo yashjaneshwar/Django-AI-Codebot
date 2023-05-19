8b3fd0ac9b99305f8bdde4a19e1b10936c6a58f8
Yash <yash8135512@gmail.com>
2023-05-19 06:59:38 +0530

    Created form action

--- codebotApp/views.py
+++ codebotApp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'docker', 'go', 'http', 'java', 'javascript', 'json', 'json5', 'jsx', 'markup', 'markup-templating', 'perl', 'php', 'plsql', 'powershell', 'python', 'regex', 'sql', 'tsx', 'typescript', 'yaml']
+    lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'docker', 'go','html', 'java', 'javascript', 'json', 'json5', 'jsx', 'markup', 'markup-templating', 'perl', 'php', 'plsql', 'powershell', 'python', 'regex', 'sql', 'tsx', 'typescript', 'yaml']
     
+    if request.method == "POST":
+        code = request.POST['code']
+        lang = request.POST['lang']
+        return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
+
     return render(request, 'home.html', {'lang_list': lang_list})
