28a662f4633385c3d3277f3c330b808416c67db6
Yash <yash8135512@gmail.com>
2023-05-19 06:21:03 +0530

    Added select box for choosing language

--- codebotApp/views.py
+++ codebotApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'docker', 'go', 'http', 'java', 'javascript', 'json', 'json5', 'jsx', 'markup', 'markup-templating', 'perl', 'php', 'plsql', 'powershell', 'python', 'regex', 'sql', 'tsx', 'typescript', 'yaml']
+    
+    return render(request, 'home.html', {'lang_list': lang_list})
