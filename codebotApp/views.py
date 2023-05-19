5f00cc6a48eca49555fd03c9738d587bca7f2f07
Yash <yash8135512@gmail.com>
2023-05-19 07:43:07 +0530

    Handled null select in form

--- codebotApp/views.py
+++ codebotApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 def home(request):
     lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'docker', 'go','html', 'java', 'javascript', 'json', 'json5', 'jsx', 'markup', 'markup-templating', 'perl', 'php', 'plsql', 'powershell', 'python', 'regex', 'sql', 'tsx', 'typescript', 'yaml']
@@ -6,6 +7,11 @@ def home(request):
     if request.method == "POST":
         code = request.POST['code']
         lang = request.POST['lang']
+
+        if lang == "Select Programming Language":
+            messages.success(request, 'Hey! You forgot to pick a programming language...')
+            return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
+
         return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
 
     return render(request, 'home.html', {'lang_list': lang_list})
